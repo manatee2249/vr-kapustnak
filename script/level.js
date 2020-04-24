54eeee536158d32baa86b2b1e59bf28af7ec1512
manatee2249 <tobias.dev2249@gmail.com>
2020-04-24 16:48:43 +0200

    update

--- script/level.js
+++ script/level.js
@@ -1,10 +1,15 @@
 
 var level = 0;
+
 function levelPlus(){
     level++;
     document.getElementById("point").innerHTML = level;
 }
 
 function more(){
-    alert("Ahoj, Právě můžeš pomoct kapustňákovy :) \n Chěl by si s tebou pohrát, najíst se, zkrátka se potřebuje s tebou zabavit.");
+    alert("Ahoj, Právě můžeš pomoct kapustňákovy :) \n Chěl by si s tebou pohrát, najíst se, \n zkrátka se potřebuje s tebou zabavit.");
+}
+
+function goTo(){
+    window.location = "sett.html";
 }
